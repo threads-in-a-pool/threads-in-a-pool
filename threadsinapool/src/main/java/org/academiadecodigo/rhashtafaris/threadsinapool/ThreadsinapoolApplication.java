ea1340174755dcff3f17ac724e15c8b46e0ca8ba
kakacordovil <kakacordovil@gmail.com>
2020-04-15 15:27:37 +0100

    first commit

---
+++ threadsinapool/src/main/java/org/academiadecodigo/rhashtafaris/threadsinapool/ThreadsinapoolApplication.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.rhashtafaris.threadsinapool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadsinapoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadsinapoolApplication.class, args);
+	}
+
+}
---
+++ threadsinapool/src/test/java/org/academiadecodigo/rhashtafaris/threadsinapool/ThreadsinapoolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.rhashtafaris.threadsinapool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadsinapoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
