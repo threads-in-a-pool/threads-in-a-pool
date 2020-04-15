d90be090c2c564910d5180724e47c3ac6e7f9173
RodrigoMorcela <rgm_99_@sapo.pt>
2020-04-15 22:51:06 +0100

    Adding Repository anotation on daos

--- threadsinapool/src/main/java/org/academiadecodigo/rhashtafaris/threadsinapool/persistence/dao/jpa/UserDaoImpl.java
+++ threadsinapool/src/main/java/org/academiadecodigo/rhashtafaris/threadsinapool/persistence/dao/jpa/UserDaoImpl.java
@@ -3,12 +3,14 @@ package org.academiadecodigo.rhashtafaris.threadsinapool.persistence.dao.jpa;
 import org.academiadecodigo.rhashtafaris.threadsinapool.model.impl.Ticket;
 import org.academiadecodigo.rhashtafaris.threadsinapool.model.impl.User;
 import org.academiadecodigo.rhashtafaris.threadsinapool.persistence.dao.UserDao;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 
+@Repository
 public class UserDaoImpl extends AbstractDao<User> implements UserDao {
 
 
