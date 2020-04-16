ea95ee4e04790d471a140c90796e00b7588c2317
kakacordovil <kakacordovil@gmail.com>
2020-04-16 18:43:36 +0100

    frontend directory

---
+++ kakathon frontend/principal.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+	$(document).on('onClick', '.container', function (e) {
+		alert('clicked');
+	});
+	$(document).on('onClick', '#tap', function (e) {
+		alert('clicked');
+	});
+});
