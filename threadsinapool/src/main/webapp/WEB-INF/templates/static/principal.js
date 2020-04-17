5c0fa53c92f519919330a6de2b12c8a37de7a6a2
Miguel Duarte <miguelpduarte13@gmail.com>
2020-04-17 06:47:52 +0100

    update

--- threadsinapool/src/main/webapp/WEB-INF/templates/static/principal.js
+++ threadsinapool/src/main/webapp/WEB-INF/templates/static/principal.js
@@ -25,11 +25,11 @@ $(document).ready(function () {
 
     function populateCarosel(events) {
 
-        $('#1').attr("src", events[1].imageURL);
-        $('#2').attr("src", events[2].imageURL);
-        $('#3').attr("src", events[3].imageURL);
-        $('#4').attr("src", events[4].imageURL);
-        $('#5').attr("src", events[5].imageURL);
+        $('#1').attr("src", events[1].imageUrl);
+        $('#2').attr("src", events[2].imageUrl);
+        $('#3').attr("src", events[3].imageUrl);
+        $('#4').attr("src", events[4].imageUrl);
+        $('#5').attr("src", events[5].imageUrl);
     }
 
     populateCarosel(events);
